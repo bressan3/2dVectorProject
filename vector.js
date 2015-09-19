f4876cc2ac479cdaa238f4b291d24f83232a08cc
Lucas B <bressan3@gmail.com>
2015-09-19 13:14:30 -0400

    Updated

--- vector.js
+++ vector.js
@@ -25,10 +25,12 @@ function vector(){
 	];
 }
 
-function drawVector(x, y, z){
+function drawVector(x, y, z, color){
 	context.beginPath();
 	context.moveTo(0,0);
 	context.lineTo(x, y, z);
+	if(color == undefined) color = '#000000';
+	context.strokeStyle = color;
 	context.stroke();
 	context.closePath();
 }
@@ -39,9 +41,13 @@ function drawPoint(x, y){
 	context.closePath();
 }
 
-function getMagnitude(){
+function getMagnitude(v1){
 	vector();
-	result.innerHTML = "Magnitude: "+Math.sqrt(Math.pow(cord1[0],2)+Math.pow(cord1[1],2)+Math.pow(cord1[2],2));
+	return +Math.sqrt(Math.pow(cord1[0],2)+Math.pow(cord1[1],2)+Math.pow(cord1[2],2));
+}
+
+function printMagnitude(){
+	restult.innerHTML = "Magnitude: "+getMagnitude();
 }
 
 function scalarMultiplication(){
@@ -101,5 +107,56 @@ function pointVecAdd(){
 
 function pointSubtraction(){
 	vector();
-	if(vector)
+
+	if(cord1[3] == 1 && cord2[3] == 1){
+		var finalVect = [
+			+cord1[0] - +cord2[0],
+			+cord1[1] - +cord2[1],
+			+cord1[2] - +cord2[2],
+		];
+
+		context.clearRect(0,0,500,500);
+		drawPoint(cord1[0], cord1[1]);
+		drawPoint(cord2[0], cord2[1]);
+		drawVector(finalVect[0], finalVect[1], finalVect[2], '#ff0000');
+		result.innerHTML = "P1 - P2 = ("+finalVect[0]+","+finalVect[1]+","+finalVect[2]+")";
+	} else result.innerHTML = "Invalid operation. Please insert two points.";
 }
+
+function crossProduct(){
+	vector();
+
+	//V1 x V2 = {y1 z2 - z1 y2; z1 x2 - x1 z2; x1 y2 - y1 x2}
+	if(cord1[3] == 0 && cord2[3] == 0){
+		var finalVect = [
+			(+cord1[1] * +cord2[2]) - (+cord1[2] * +cord2[1]),
+			(+cord1[2] * +cord2[0]) - (+cord1[0] * +cord2[2]),
+			(+cord1[0] * +cord2[1]) - (+cord1[1] * +cord2[0])
+		];
+
+		result.innerHTML = "V1 x V2 = ("+finalVect[0]+","+finalVect[1]+","+finalVect[2]+")";
+	} else result.innerHTML = "Invalid operation. Please insert two vectors to continue.";
+}
+
+function dotProduct(){
+	vector();
+	result.innerHTML = "V1/P1 . V2/P2 = ("+cord1[0]*cord2[0]+","+cord1[1]*cord2[1]+","+cord1[2]*cord2[2]+")";
+}
+
+function normalizeVect(){
+	vector();
+
+	if(cord1[3] == 0){
+		var finalVect = [
+			+cord1[0]/+getMagnitude(),
+			+cord1[1]/+getMagnitude(),
+			+cord1[2]/+getMagnitude()
+		];
+
+		context.clearRect(0,0,500,500);
+		drawVector(finalVect[0], finalVect[1], finalVect[2], '#ff0000');
+		result.innerHTML = "V1(n) = ("+finalVect[0]+","+finalVect[1]+","+finalVect[2]+")";
+	} else result.innerHTML = "Invalid operation. V1 is not a vector.";
+
+}
+
